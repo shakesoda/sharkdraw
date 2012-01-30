b575e18d97c6f6a27af5db6e67fcaa630686999a
Colby Klein <shakesoda@gmail.com>
2012-01-30 11:31:11 -0800

    fix painting when canvas isn't centered. add some speed scaling for testing, the math looks a little ugly and needs some helpers. need to add substeps to keep things smooth.

--- client/js/sharkdraw.js
+++ client/js/sharkdraw.js
@@ -9,12 +9,13 @@ function sd(el, mode) {
 	document.body.addEventListener("mousemove", this.draw.bind(this))
 	document.body.addEventListener("mouseup", this.stop.bind(this))
 
-	this.previous = { x: 0, y: 0 }
+	this.previous = { x: 0, y: 0, width: 0 }
+	this.position = this.canvas.getBoundingClientRect()
 }
 
 sd.prototype.start = function(e) {
-	this.previous.x = e.pageX
-	this.previous.y = e.pageY
+	this.previous.x = e.pageX - this.position.left
+	this.previous.y = e.pageY - this.position.top
 	this.painting = true
 	this.draw(e)
 }
@@ -27,18 +28,38 @@ sd.prototype.draw = function(e) {
 	if (!this.painting)
 		return
 
+	var pos = { x: e.pageX - this.position.left, y: e.pageY - this.position.top }
+	var diff = [ Math.abs(this.previous.x - pos.x), Math.abs(this.previous.y - pos.y) ]
+	var speed = Math.sqrt(diff[0] * diff[0] + diff[1] * diff[1])
+
+	if (speed > 25) {
+		// substeps needed when moving fast or lines get blocky
+		console.log("needs steps")
+	}
+
 	var c = this.ctx
 
 	c.lineWidth = 10;
 	c.lineCap = "round";
 
+	// badly needs interpolation to be usable
+	if (this.options.speedScaling)
+	{
+		var scale = speed / 10
+		scale = Math.max(scale, 5)
+		scale = Math.min(scale, 20)
+
+		c.lineWidth = scale
+		this.previous.width = scale
+	}
+
 	c.beginPath();
 	c.moveTo(this.previous.x, this.previous.y);
-	c.lineTo(e.pageX,e.pageY);
+	c.lineTo(pos.x, pos.y);
 	c.stroke();
 
-	this.previous.x = e.pageX
-	this.previous.y = e.pageY
+	this.previous.x = pos.x
+	this.previous.y = pos.y
 }
 
 sd.prototype.clear = function() {
